b5ddd7536e4fe5fcf6df8cda2939ce8fb57fb77b
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-09-10 13:06:21 +0100

    Added packet analysis functionality

--- packet_capturer.py
+++ packet_capturer.py
@@ -8,8 +8,14 @@ iface = "en0"
 def packet_filter(packet):
     return IPv4 in packet
 
+def analyse_packet(packet):
+    if IPv4 in packet:
+        source_ip = packet[IPv4].src
+        destination_ip = packet[IPv4].dst
+        print(f"Source IP: {source_ip}, Destination IP: {destination_ip}")
+
 # Sniff packets and apply the filter
-packets = sniff(iface=iface, filter="ip", lfilter=packet_filter, count=10)
+packets = sniff(iface=iface, filter="ip", lfilter=packet_filter, prn=analyse_packet, count=10)
 
 # Print or log the filtered packets
 for packet in packets:
