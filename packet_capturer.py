e3fd3963051176047bc1dcc41c66675655d10c6c
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-09-10 12:58:50 +0100

    Implemented packet filtering functionality

---
+++ packet_capturer.py
@@ -0,0 +1,16 @@
+from scapy.all import sniff
+from scapy.layers.inet import IP as IPv4
+
+# Define the network interface to capture packets from
+iface = "en0"
+
+# Function to filter packets by source IP address
+def packet_filter(packet):
+    return IPv4 in packet
+
+# Sniff packets and apply the filter
+packets = sniff(iface=iface, filter="ip", lfilter=packet_filter, count=10)
+
+# Print or log the filtered packets
+for packet in packets:
+    print(packet.summary())
